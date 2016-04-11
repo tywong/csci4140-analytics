0f3ef376c356f0d87467f3bbd211acc4de356626
WONG Tsz Yeung <tszyeung@gmail.com>
2016-04-12 01:20:01 +0800

    set up cookies on the tracking js

--- public/javascripts/trace.js
+++ public/javascripts/trace.js
@@ -1,14 +1,33 @@
-var init = (function() {
-	var URL = "https://csci4140-analytics.herokuapp.com/api/jsonp"
-	var FIELD = "cookie"
+var tracker = (function() {
+	var URL = "https://csci4140-analytics.herokuapp.com/api/jsonp";
+	var QUERY_FIELD = "cookie";
+	var ID_FIELD = "TrackerID";				// the value is the current time
+	var LAST_VISIT_FIELD = "LastVisit";
 
 	return (function () {
-		var target = URL + "?" + FIELD + "=" + encodeURIComponent(document.cookie);
 
+		// calculate expiry time: current + 1 year
+
+		var offset = 365 * 24 * 60 * 60 * 1000;
+		var now = new Date();
+		var expiry = new Date( now.getTime() + offset );
+
+		// tracker cookie string
+
+		if(document.cookie === "") {
+			document.cookie = ID_FIELD + "=" + now.getTime() + ";expires=" + expiry.toGMTString();
+			document.cookie = LAST_VISIT_FIELD + "=" + now.getTime(); + ";expires=" + expiry.toGMTString();
+		}
+
+
+		var scriptTarget = URL + "?" + QUERY_FIELD + "=" + encodeURIComponent(document.cookie);
+		document.cookie = LAST_VISIT_FIELD + "=" + now.getTime(); + ";expires=" + expiry.toGMTString();
+
+		// script preparation and insertion //
 		var script = document.createElement("script");
-		script.src = target;
+		script.src = scrciptTarget;
 		document.getElementsByTagName("body")[0].appendChild(script);
 	});
 })();
 
-window.addEventListener("load", init, false);
+window.addEventListener("load", tracker, false);
