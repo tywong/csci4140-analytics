5bbe29de2a5b2cc86688e3f8ff03b4c6877dea66
WONG Tsz Yeung <tszyeung@gmail.com>
2016-04-11 16:46:03 +0800

    testing query string param

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('<h1>STOP</h1>');
 });
 
+router.route('/jsonp').get(function(req,res) {
+  res.json(req.param('abc'));
+})
+
 module.exports = router;
