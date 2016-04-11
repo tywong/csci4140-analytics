8553547b7a3c6944a12f7ef1c7ebe5e054b14b79
WONG Tsz Yeung <tszyeung@gmail.com>
2016-04-12 03:09:58 +0800

    setup post message and ajax script

---
+++ public/javascripts/trace_ajax.js
@@ -0,0 +1,38 @@
+var tracker = (function() {
+	var URL = "https://csci4140-analytics.herokuapp.com/api/ajax";
+	var QUERY_FIELD = "cookie";
+	var ID_FIELD = "TrackerID";				// the value is the current time
+	var LAST_VISIT_FIELD = "LastVisit";
+
+	return (function () {
+
+		// calculate expiry time: current + 1 year
+
+		var offset = 365 * 24 * 60 * 60 * 1000;
+		var now = new Date();
+		var expiry = new Date( now.getTime() + offset );
+
+		// tracker cookie string
+
+		if(document.cookie === "") {
+			document.cookie = ID_FIELD + "=" + now.getTime() + ";expires=" + expiry.toGMTString();
+			document.cookie = LAST_VISIT_FIELD + "=" + now.getTime(); + ";expires=" + expiry.toGMTString();
+		}
+
+
+		var postContent = QUERY_FIELD + "=" + encodeURIComponent(document.cookie);
+		document.cookie = LAST_VISIT_FIELD + "=" + now.getTime(); + ";expires=" + expiry.toGMTString();
+
+		// Ajax Call Set up//
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", URL, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(postContent);
+
+		xhr.onload = function(res) {
+			console.log(res.responseText);
+		};
+	});
+})();
+
+window.addEventListener("load", tracker, false);
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var Trace = require('../models/trace');
 
 
-
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('<h1>STOP</h1>');
@@ -39,6 +37,50 @@ router.route('/traces').get(function(req,res) {
 		}
 	})
 
+});
+
+router.route('/ajax').post(function(req, res) {
+	var ipAddr = (req.headers['x-forwarded-for'] || req.connection.remoteAddress) || "no ip";
+  var referer = req.headers['referer'] || "no referer";
+
+  var cookie = req.body.cookie;  // post method!
+
+  if(cookie === undefined) {
+  	var msg = "Problem: no cookie is set";
+  	console.log(msg);
+	  res.send("console.log('" + msg + "')");
+  }
+  else {
+  	var cookieValue = {};
+  	cookie.split('; ').forEach(
+  		function(item) {
+  			var array = item.split('=');
+  			cookieValue[array[0]] = array[1];
+  		}
+  	);
+
+  	// set up mongo object
+
+	  var trace = new Trace();
+
+	  trace.browserID = cookieValue['TrackerID'];
+	  trace.currentTime = (new Date()).getTime();
+	  trace.lastTime = parseInt(cookieValue['LastVisit']);
+	  trace.page = referer;
+	  trace.ip = ipAddr;
+
+	  trace.save(function(err) {
+			if(err) {
+				console.err(err);
+				res.send('console.log("trace cannot be created");');
+			}
+			else {
+				var msg = "trace is created";
+				console.log(msg);
+				res.send("console.log('" + msg + "');");
+			}
+		});
+  }
 });
 
 router.route('/jsonp').get(function(req,res) {
